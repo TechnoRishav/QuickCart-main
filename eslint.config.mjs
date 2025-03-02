bd6cb700504508a6d46d525c596ff5cc405cc462
TechnoRishav <rishav2004.taral@gmail.com>
2025-03-02 20:07:15 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
 
 export default eslintConfig;
